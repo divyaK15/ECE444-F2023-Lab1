3e707654241af959e74e654ed4feddc158d65988
divyaK15 <divya.p.k15@gmail.com>
2023-09-19 10:50:19 -0400

    added form flask

--- hello.py
+++ hello.py
@@ -3,15 +3,27 @@ from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime as datet
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = "hardString"
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
 @app.route('/')
 def index():
-    print("Hello World")
-    print(datet.utcnow())
-    return render_template('index.html', current_time=datet.utcnow())
+    name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 
 @app.route('/user/<name>')
