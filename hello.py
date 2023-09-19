a5483704b43040e6edf97d936b0c51fe2fe93ee6
divyaK15 <divya.p.k15@gmail.com>
2023-09-19 11:16:55 -0400

    recreated example 4-7

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from datetime import datetime as datet
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask import Flask, render_template, session, redirect, url_for, flash
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -16,14 +17,16 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',form = form, name = session.get('name'))
 
 
 @app.route('/user/<name>')
