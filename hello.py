cd9bf63bd6f4788806ee33b07677bd8dff90f885
divyaK15 <divya.p.k15@gmail.com>
2023-09-21 15:54:25 -0400

    added email

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from datetime import datetime as datet
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from wtforms.validators import Email, DataRequired
 from flask import Flask, render_template, session, redirect, url_for, flash
 
 app = Flask(__name__)
@@ -15,18 +16,25 @@ app.config['SECRET_KEY'] = "hardString"
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
+    email = StringField('What is your UofT Email Address?', validators=[DataRequired(), Email()])
     submit = SubmitField('Submit')
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
+        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
+        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html',form = form, name = session.get('name'))
+    return render_template('index.html',form = form, name = session.get('name'), email = session.get('email'))
 
 
 @app.route('/user/<name>')
